e4aba8687168988bc987758ebd4c6488a34a4887
Momen Ellaban <68505187+MomenEllaban@users.noreply.github.com>
2023-08-02 20:36:56 +0300

    fix erorr

--- src/context/ShopSelectedContext.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-
-export const ShopSelectedContext = createContext({
-  selectedShop: 0,
-  setSelectedShop: (selectedShop: any) => {},
-});
--- src/utils/app-contants.ts
+++
@@ -1,8 +0,0 @@
-export const ELocalStorageKeys = {
-  TOKEN: 'userinfo',
-  TOKEN_COOKIE: 'tokend',
-  USER_NAME: 'username',
-  USER_LOCATIONS: 'userlocs',
-  FULL_NAME: 'userfullname',
-  LEVELS: 'levels',
-};
