bf550b4e2d310582bd0efb7e64147e6df80d510e
Momen Ellaban <68505187+MomenEllaban@users.noreply.github.com>
2023-08-03 01:15:34 +0300

    replace

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export { default as ImageFallback } from './Image/ImageFallback';
-export { default as Layout } from './layout/layout';
--- src/components/providers/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as UserProvider } from './UserProvider';
-export { default as ProductProvider } from './ProductProvider';
