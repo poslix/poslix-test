8494f5b6de9e975387a1922203caa8b4d1904df6
Momen Ellaban <68505187+MomenEllaban@users.noreply.github.com>
2023-08-07 11:38:57 +0300

    replace

--- src/layout/index.ts
+++
@@ -1,3 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export { default as AdminLayout } from './AdminLayout/AdminLayout'
-export { default as OwnerAdminLayout } from './AdminLayout/OwnerAdminLayout'
