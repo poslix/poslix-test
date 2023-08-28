2d241a1bf758193b7d908e074ec16dba65d7ef2d
Momen Ellaban <68505187+MomenEllaban@users.noreply.github.com>
2023-08-29 01:03:37 +0300

    lol2

--- src/utils/app-constants.ts
+++
@@ -1,9 +0,0 @@
-export const ELocalStorageKeys = {
-  TOKEN: 'userinfo',
-  TOKEN_COOKIE: 'tokend',
-  USER: 'userdata',
-  USER_NAME: 'username',
-  USER_LOCATIONS: 'userlocs',
-  FULL_NAME: 'userfullname',
-  LEVELS: 'levels',
-};
