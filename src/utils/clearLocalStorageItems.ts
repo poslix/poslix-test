5060eb6d3ce007300cb51e09fd514c45fc2be3a3
Momen Ellaban <68505187+MomenEllaban@users.noreply.github.com>
2023-08-29 19:32:12 +0300

    ok

--- src/utils/clearLocalStorageItems.ts
+++
@@ -1,8 +0,0 @@
-export function clearLocalStorageItems() {
-  localStorage.removeItem('userlocs');
-  localStorage.removeItem('userinfo');
-  localStorage.removeItem('userfullname');
-  localStorage.removeItem('username');
-  localStorage.removeItem('levels');
-  localStorage.removeItem('orders');
-}
